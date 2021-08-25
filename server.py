fa7e2716f7bb42dd240f5c3189bfceee73783644
Raghav Sharma <raghav_tinker@yahoo.com>
2021-08-25 22:25:18 +0530

    port issue

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run("app.main:app", 
                 host="0.0.0.0", 
-                port=9000,
+                port=5000,
                 reload=True,
                 ssl_keyfile="./raghavtinker.servatom.com-key.pem",
                 ssl_certfile="./raghavtinker.servatom.com.pem"
